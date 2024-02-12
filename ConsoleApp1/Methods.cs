2bd8a1632d04f79df1a594b41708a6ff14d88065
Ismoil Tohirov <ismoiltohirov55@gmail.com>
2024-02-12 08:51:43 +0500

    Add project files.

---
+++ ConsoleApp1/Methods.cs
@@ -0,0 +1,313 @@
+﻿using Npgsql;
+using System.Net.Quic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using static Npgsql.Replication.PgOutput.Messages.RelationMessage;
+
+namespace ConsoleApp1
+{
+    public class Methods
+    {
+        public static void CreateTable(string table, string column)
+        {
+            string connetionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+            
+            using (NpgsqlConnection connection = new NpgsqlConnection(connetionString))
+            {
+                connection.Open();
+
+                string query = @$"CREATE TABLE {table} (id SERIAl, {column} VARCHAR(255));";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query,connection);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Succesfully Created !");
+
+                connection.Close();
+            }
+        }
+
+        public static void GetAll(string table)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"SELECT * FROM {table};";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+                var result = cmd.ExecuteReader();
+
+                Console.WriteLine();
+
+                while (result.Read())
+                {
+                    Console.WriteLine(result[1
+                        ]);
+                }
+
+                Console.WriteLine("\nOperation is succesfully ended !");
+                connection.Close();
+            }
+        }
+        public static void GetById(string table, int id)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"SELECT * FROM {table} WHERE id = {id};";
+
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+                
+                var result = cmd.ExecuteReader();
+
+                Console.WriteLine();
+
+                while (result.Read())
+                {
+                    Console.WriteLine(result[1]);
+                }
+
+                Console.WriteLine("\nOperation is succesfully ended !");
+                connection.Close();
+            }
+        }
+        public static void GetWithLike(string table, string column, string like)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"SELECT * FROM {table} WHERE {column} LIKE '{like}';";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+                var result = cmd.ExecuteReader();
+
+                while (result.Read())
+                {
+                    Console.WriteLine(result);
+                }
+
+                Console.WriteLine("Operation is succesfully ended !");
+
+                connection.Close();
+            }
+        }
+
+
+        public static void UpdateTable(string oldTableName, string NewTableName)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+            using( NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"ALTER TABLE {oldTableName} RENAME TO {NewTableName};";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query,connection);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Succesfully updated !");
+
+                connection.Close();
+            }
+        }
+        public static void UpdateAll(string table, string columnName, string value)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"UPDATE {table} SET {columnName} = '{value}';";
+                using(NpgsqlCommand cmd =  new NpgsqlCommand(query,connection))
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Succesfully updated !");
+
+                connection.Close();
+            }
+        }
+        public static void UpdateOne(string table, string columnName, string value, string columnName2, string value2)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"UPDATE {table} SET {columnName} = '{value}' WHERE {columnName2} = '{value2}';";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Succesfully updated !");
+
+                connection.Close();
+            }
+        }
+        public static void UpdateColumn(string table, string oldname, string newname)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"ALTER TABLE {table} RENAME COLUMN {oldname} TO {newname};";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query,connection);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Succesfully Updated !");
+
+                connection.Close();
+            }
+        }
+        public static void DeleteFrom(string table, string columnName, string value)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"DELETE FROM {table} WHERE {columnName} = '{value}';";
+
+                using(NpgsqlCommand cmd = new NpgsqlCommand(query,connection))
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Succesfully deleted !");
+
+                connection.Close();
+            }
+        }
+        public static void TruncateTable(string table)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = $@"TRUNCATE TABLE {table};";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, connection))
+
+                cmd.ExecuteNonQuery();
+                
+                Console.WriteLine("Operation is succesfully ended !");
+                
+                connection.Close();
+            }
+        }
+
+
+        public static void InsertOne(string table, string columnName, string value)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"INSERT INTO {table}({columnName}) VALUES('{value}');";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+                
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Succesfully Added!");
+                
+                connection.Close();
+            }
+        }
+
+
+        public static void AddColumn(string table, string column, string type)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"ALTER TABLE {table} ADD COLUMN {column} {type};";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("the new column is succesfully Added !");
+
+                connection.Close();
+            }
+        }
+        public static void AddColumnWithDefault(string table, string column, string type)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"ALTER TABLE {table} ADD COLUMN {column} {type} DEFAULT 'Example';";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("the new column is succesfully Added !");
+
+                connection.Close();
+            }
+        }
+        public static void Join(string table, string table2, string join1, string join2)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"SELECT * FROM {table} INNER JOIN {table2} ON {join1} = {join2};";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+                var result = cmd.ExecuteReader();
+
+                while (result.Read())
+                {
+                    Console.Write(result[0]);
+                    Console.WriteLine(result[1]);
+                }
+
+                Console.WriteLine("Operation is succesfully ended !");
+
+                connection.Close();
+            }
+        }
+        public static void AddIndex(string index, string table, string column)
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"CREATE INDEX {index} ON {table} ({column});";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Succesfully Added !");
+
+                connection.Close();
+            }
+        }
+        public static void InsertMany()
+        {
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/Paginaton.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Paginaton
+    {
+        public static void Page1()
+        {
+            Console.WriteLine("PAGE 1\n");
+            Console.WriteLine("1) create table");
+            Console.WriteLine("2) GetAll");
+            Console.WriteLine("3) InsertOne");
+            Console.WriteLine("4) Truncate table");
+            Console.WriteLine("5) UpdateAll");
+            Console.WriteLine("6) Clear console...");
+            Console.WriteLine("7) Exit...");
+            Console.WriteLine("8) Next page");
+            Console.WriteLine("Choose one of this functions (1, 2, or 3)... ");
+
+            var result = Console.ReadLine();
+
+            int res = Int32.Parse(result);
+
+            switch (res)
+            {
+                case 1:
+                    Console.WriteLine("Enter table name and column.");
+
+                    Methods.CreateTable(Console.ReadLine(), Console.ReadLine());
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter the table name.");
+
+                    Methods.GetAll(Console.ReadLine());
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter the table name, columnName and value.");
+
+                    Methods.InsertOne(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+                    break;
+
+                case 4:
+                    Console.WriteLine("Enter the table name.");
+
+                    Methods.TruncateTable(Console.ReadLine());
+                    break;
+
+                case 5:
+                    Console.WriteLine("Enter the table, columnName and value.");
+
+                    Methods.UpdateAll(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+                    break;
+
+                case 6:
+                    Console.Clear();
+                    break;
+
+                case 7:
+                    Console.WriteLine("OK");
+                    break;
+
+                case 8:
+                    Console.Clear();
+                    Page2();
+                    break;
+
+                default:
+                    Console.WriteLine("Please, enter only existed numbers in the page!\n");
+                    break;
+
+            }
+            if (res != 7)
+            {
+                Page1();
+            }
+            else
+            {
+                Console.WriteLine("See you later :)");
+            }
+        }
+        public static void Page2()
+        {
+            Console.WriteLine("Page 2\n");
+            Console.WriteLine("1) DeleteFrom");
+            Console.WriteLine("2) GetById");
+            Console.WriteLine("3) UpdateOne");
+            Console.WriteLine("4) UpdateColumnName");
+            Console.WriteLine("5) Clear console...");
+            Console.WriteLine("6) Exit...");
+            Console.WriteLine("7) Previous page || 8) Next Page ");
+            Console.WriteLine("Choose one of this functions (1, 2, or 3)... ");
+            var result = Console.ReadLine();
+
+            int res = Int32.Parse(result);
+
+            switch (res)
+            {
+                case 1:
+                    Console.WriteLine("Enter the table, column and value");
+
+                    Methods.DeleteFrom(Console.ReadLine(),Console.ReadLine(),Console.ReadLine());
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter the table, column and id");
+
+                    Methods.DeleteFrom(Console.ReadLine(),Console.ReadLine(),Console.ReadLine());
+                    break;
+
+                case 3: 
+                    Console.WriteLine("Enter the table, column, value, (WHERE) column, and value");
+
+                    Methods.UpdateOne(Console.ReadLine(),Console.ReadLine(),Console.ReadLine(),Console.ReadLine(),Console.ReadLine());
+                    break;
+
+                case 4:
+                    Console.WriteLine("Enter the table, oldColumnName and NewColumnName");
+
+                    Methods.UpdateColumn(Console.ReadLine(),Console.ReadLine(),Console.ReadLine());
+                    break;
+
+                case 5:
+                    Console.Clear();
+                    break;
+
+                case 6:
+                    Console.WriteLine("OK");
+                    break;
+
+                case 7:
+                    Console.Clear();
+                    Page1();
+                    break;
+
+                case 8:
+                    Console.Clear();
+                    Page3();
+                    break;
+
+                default:
+                    Console.WriteLine("Please, enter only Existed numbers in the page!");
+                    break;
+
+            }
+            if (res != 6)
+            {
+                Page2();
+            }
+            else
+            {
+                Console.WriteLine("See you later :)");
+            }
+        }
+        public static void Page3()
+        {
+            Console.WriteLine("Page 3\n");
+            Console.WriteLine("1) AddColumn");
+            Console.WriteLine("2) AddColumnDefault");
+            Console.WriteLine("3) UpdateTableName");
+            Console.WriteLine("4) Join");
+            Console.WriteLine("5) AddIndex");
+            Console.WriteLine("6) Clear console...");
+            Console.WriteLine("7) Exit...");
+            Console.WriteLine("8) Previous page");
+            Console.WriteLine("Choose one of this functions (1, 2, or 3)... ");
+            var result = Console.ReadLine();
+
+            int res = Int32.Parse(result);
+
+            switch (res)
+            {
+                case 1:
+                    Console.WriteLine("Enter the table, column and value");
+
+                    Methods.AddColumn(Console.ReadLine(),Console.ReadLine(),Console.ReadLine());
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter  old table name and new table name.");
+
+                    Methods.UpdateTable(Console.ReadLine(), Console.ReadLine());
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter the table, column and value");
+
+                    Methods.AddColumn(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+                    break;
+
+                case 4:
+                    Console.WriteLine("Enter the first table, second table, join1 and join2");
+
+                    Methods.Join(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(),Console.ReadLine());
+                    break;
+
+                case 5:
+                    Console.WriteLine("Enter the Index, table and value");
+
+                    Methods.AddIndex(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+                    break;
+
+                case 6:
+                    Console.Clear();
+                    Page3();
+                    break;
+
+                case 7:
+                    Console.WriteLine("OK");
+                    break;
+
+                case 8:
+                    Console.Clear();
+                    Page2();
+                    break;
+
+                default:
+                    Console.WriteLine("Please, Enter only existed numbers in the page !");
+                    break;
+            }
+            if (res != 7)
+            {
+                Page3();
+            }
+            else
+            {
+                Console.WriteLine("See you later :)");
+            }
+
+        }
+    }
+}
